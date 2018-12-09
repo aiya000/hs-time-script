a633bd255502b4d5f3fa45aa8e529054eff7c47b
aiya000 <aiya000.develop@gmail.com>
2018-12-09 21:59:27 +0900

    :+1: Initialize as a stack project

---
+++ app/Main.hs
@@ -0,0 +1,8 @@
+module Main where
+
+import RIO
+import Tonatona (run)
+import Tims.Main (app)
+
+main :: IO ()
+main = run app
---
+++ src/Tims/Main.hs
@@ -0,0 +1,22 @@
+-- | Exposes the standard entry point
+module Tims.Main where
+
+import RIO
+import Tonatona (HasConfig(..), HasParser(..))
+import qualified Tonatona.Logger as TonaLogger
+
+-- | The standard entry point
+app :: RIO Config ()
+app = do
+  logInfo $ display ("This is a skeleton for tonatona project" :: Text)
+  logDebug $ display ("This is a debug message" :: Text)
+
+newtype Config = Config
+  { tonaLogger :: TonaLogger.Config
+  }
+
+instance HasConfig Config TonaLogger.Config where
+  config = tonaLogger
+
+instance HasParser Config where
+  parser = Config <$> parser
