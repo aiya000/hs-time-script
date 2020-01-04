248fe898e6e343629399431c04775866688d1ada
aiya000 <aiya000.develop@gmail.com>
2020-01-04 19:57:28 +0900

    :+1: Make the small REPL only to print ASTs
    ```
    > :ast let x = 10
    Code [Let (LVar (SimpleLocal "x")) Nothing (RLit (Nat 10))]
    
    > :ast 10
    Rhs (RLit (Nat 10))
    ```

--- src/Tim/Main.hs
+++ src/Tim/Main.hs
@@ -1,16 +1,17 @@
 -- | Exposes the standard entry point
 module Tim.Main where
 
-import RIO hiding (logInfo, logDebug)
+import Data.Bifunctor (first)
+import qualified Data.Text as Text
+import Data.Text.Prettyprint.Doc (pretty)
+import Prelude (putStrLn, print)
+import RIO hiding (logInfo, logDebug, first)
+import System.Console.Haskeline
+import Tim.Lexer (lex)
+import Tim.Parser (parse)
+import Tim.Parser.Types (AST)
 import Tonatona (HasConfig(..), HasParser(..))
-import Tonatona.Logger (logInfo, logDebug)
 import qualified Tonatona.Logger as TonaLogger
-
--- | The standard entry point
-app :: RIO Config ()
-app = do
-  logInfo $ display @Text "This is a skeleton for tonatona project"
-  logDebug $ display @Text "This is a debug message"
 
 newtype Config = Config
   { tonaLogger :: TonaLogger.Config
@@ -21,3 +22,37 @@ instance HasConfig Config TonaLogger.Config where
 
 instance HasParser Config where
   parser = Config <$> parser
+
+
+-- | The standard entry point
+app :: RIO Config ()
+app = ask >>= liftIO . runInputT defaultSettings . loop
+  where
+    loop :: Config -> InputT IO ()
+    loop env = do
+      maybeInput <- getInputLine "> "
+      case maybeInput of
+        Nothing -> pure ()
+        Just input -> do
+          liftIO . runRIO env $ resolve input
+          loop env
+
+    resolve :: String -> RIO Config ()
+    resolve xs = case words xs of
+      [] -> pure ()
+      (":ast" : args) -> printAST $ unwords args
+      (_ : _) -> liftIO . putStrLn $ "Not implemented yet: " <> xs
+
+    printAST :: String -> RIO Config ()
+    printAST code =
+      case process $ Text.pack code of
+        Left reason -> liftIO $ putStrLn reason
+        Right ast -> liftIO $ print ast
+
+
+type PrettyFailure = String
+
+process :: Text -> Either PrettyFailure AST
+process code =
+  let x = parse =<< lex code
+  in first (show . pretty) x
--- test/tasty/Tim/Test.hs
+++ test/tasty/Tim/Test.hs
@@ -1,17 +1,12 @@
 -- | Helpers for tests.
 module Tim.Test where
 
-import Data.Bifunctor (first)
 import qualified Data.Text as Text
-import Data.Text.Prettyprint.Doc (pretty)
-import RIO hiding (first)
+import RIO
 import Test.Tasty (TestName, TestTree)
 import Test.Tasty.HUnit ((@?=), Assertion, testCase)
-import Tim.Lexer (lex)
-import Tim.Parser (parse)
+import Tim.Main (process, PrettyFailure)
 import Tim.Parser.Types
-
-type PrettyFailure = String
 
 infixl 1 `shouldBe`
 
@@ -20,10 +15,5 @@ shouldBe name expected =
   testCase name $
     process (Text.pack name) `toBe` expected
   where
-    process :: Text -> Either PrettyFailure AST
-    process code =
-      let x = parse =<< lex code
-      in first (show . pretty) x
-
     toBe :: HasCallStack => Either PrettyFailure AST -> AST -> Assertion
     actual `toBe` expected' = actual @?= Right expected'
