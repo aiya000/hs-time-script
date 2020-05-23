b21a3355d9d9d0b8cfd13cd9fc2d17279af0ef21
aiya000 <aiya000.develop@gmail.com>
2020-05-23 20:25:30 +0900

    :+1: Refine Tim.Test

---
+++ test/tasty/Tim/LexerTest/Error.hs
@@ -0,0 +1,1 @@
+module Tim.LexerTest.Error where
--- test/tasty/Tim/Test.hs
+++ test/tasty/Tim/Test.hs
@@ -7,18 +7,23 @@ import RIO
 import Test.Tasty (TestName, TestTree)
 import Test.Tasty.HUnit ((@?=), Assertion, testCase)
 import Text.Megaparsec
-import Tim.Main (process, PrettyFailure)
+import Tim.Main (process)
 import Tim.Parser.Types hiding (String)
 
 infixl 1 `shouldBe`
+
+toBe :: (HasCallStack, Show e, Show a, Eq a) => Either e a -> a -> Assertion
+(Right actual) `toBe` expected = actual @?= expected
+(Left illegal) `toBe` _ = error $ "Test error! Expected a (Right _), but got: Left " <> show illegal
+
+toBeLeftOf :: (HasCallStack, Eq e, Show e, Show a) => Either e a -> e -> Assertion
+(Left actual) `toBeLeftOf` expected = actual @?= expected
+(Right illegal) `toBeLeftOf` _ = error $ "Test error! Expected a (Left _), but got: Right " <> show illegal
 
 thatShouldBe :: HasCallStack => TestName -> AST -> String -> TestTree
 thatShouldBe testName expected expr =
   testCase testName $
     process (Text.pack expr) `toBe` expected
-  where
-    toBe :: HasCallStack => Either PrettyFailure AST -> AST -> Assertion
-    actual `toBe` expected' = actual @?= Right expected'
 
 shouldBe :: HasCallStack => TestName -> AST -> TestTree
 shouldBe expr expected = thatShouldBe expr expected expr
