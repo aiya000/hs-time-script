054a4cab6a40593a934ff783ce1ed5fcb1fd02fc
aiya000 <aiya000.develop@gmail.com>
2020-05-23 23:06:24 +0900

    :+1: Add comments to choice tests

--- test/tasty/Main.hs
+++ test/tasty/Main.hs
@@ -1,1 +1,17 @@
 {-# OPTIONS_GHC -F -pgmF tasty-discover #-}
+
+-- module Main where
+-- 
+-- -- import Tim.ParserTest.Code.Function
+-- import Prelude
+-- import Test.Tasty
+-- import Tim.LexerTest.Error
+-- import Tim.ParserTest.Rhs
+-- 
+-- main :: IO ()
+-- main = defaultMain $ testGroup "selected"
+--   [ testGroup "" test_nats
+--   , testGroup "" test_floats
+--   , testGroup "" test_dicts
+--   , testGroup "" test_lexer_error_positions
+--   ]
