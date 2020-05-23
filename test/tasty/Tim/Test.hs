8da7ade38ed88f596d2633c72ddde1eee5201ad4
aiya000 <aiya000.develop@gmail.com>
2020-05-23 22:58:55 +0900

    :+1: Improve showing test results

--- test/tasty/Tim/Test.hs
+++ test/tasty/Tim/Test.hs
@@ -2,23 +2,32 @@
 module Tim.Test where
 
 import Data.String.Cases (Camel, parseCamel)
+import Data.String.Here (i)
 import qualified Data.Text as Text
 import RIO
 import Test.Tasty (TestName, TestTree)
-import Test.Tasty.HUnit ((@?=), Assertion, testCase)
+import Test.Tasty.HUnit ((@?=), Assertion, testCase, assertFailure)
 import Text.Megaparsec
 import Tim.Main (process)
 import Tim.Parser.Types hiding (String)
 
 infixl 1 `shouldBe`
 
-toBe :: (HasCallStack, Show e, Show a, Eq a) => Either e a -> a -> Assertion
+toBe :: (HasCallStack, Typeable e, Show e, Show a, Eq a) => Either e a -> a -> Assertion
 (Right actual) `toBe` expected = actual @?= expected
-(Left illegal) `toBe` _ = error $ "Test error! Expected a (Right _), but got: Left " <> show illegal
+(Left illegal) `toBe` _ =
+  assertFailure $ trimMargins
+    [i||  Expected a (Right _),
+       |  but got: Left ${illegal}
+    |]
 
-toBeLeftOf :: (HasCallStack, Eq e, Show e, Show a) => Either e a -> e -> Assertion
+toBeLeftOf :: (HasCallStack, Eq e, Show e, Typeable a, Show a) => Either e a -> e -> Assertion
 (Left actual) `toBeLeftOf` expected = actual @?= expected
-(Right illegal) `toBeLeftOf` _ = error $ "Test error! Expected a (Left _), but got: Right " <> show illegal
+(Right illegal) `toBeLeftOf` _ =
+  assertFailure $ trimMargins
+    [i||  Expected a (Left _),
+       |  but got: Right ${illegal}
+    |]
 
 thatShouldBe :: HasCallStack => TestName -> AST -> String -> TestTree
 thatShouldBe testName expected expr =
@@ -43,3 +52,9 @@ name = ignore parseCamel
 -- | Unsafe
 con :: String -> Type
 con = TypeCon . name
+
+
+trimMargins :: String -> String
+trimMargins = trim . unlines . map trim . lines
+  where
+    trim = reverse . dropWhile (== ' ') . reverse . dropWhile (== ' ')
